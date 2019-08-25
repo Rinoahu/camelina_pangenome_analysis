301d5e75c38bec5ae6dce0c5cfe5283bcd1cea71
Rinoahu <xiaohu@iastate.edu>
2019-08-25 09:57:30 -0600

    modify requirement

--- sam_flt.py
+++ sam_flt.py
@@ -19,11 +19,11 @@ _o1 = pysam.AlignmentFile(qry+'.mapped.bam', "wb", template=f)
 _o = pysam.AlignmentFile(qry+'.unmapped.bam', "wb", template=f)
 
 
-for i in f:
+for i in f.fetch(until_eof=True):
 	#if len(i.cigar) == 1 and i.cigar[0][0] == 0:
 	#if sum([elem[1] for elem in i.cigar if elem[0]!=0]) <= 10:
 	flag = sum([elem[1] for elem in i.cigar if elem[0]!=0])
-	if flag < 10:
+	if flag < 10 and i.is_unmapped == False:
 		_o1.write(i)
 	else:
 		_o.write(i)
